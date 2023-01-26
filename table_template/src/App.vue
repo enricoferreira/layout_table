768ac3595efe0eb152d62297110a13380bbd3ac6
Enrico Ferreira <enricoferreira41@gmail.com>
2023-01-26 18:36:43 -0300

    app bar component

--- table_template/src/App.vue
+++ table_template/src/App.vue
@@ -1,53 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <TheAppBar/>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
+    <v-main style="height: 10000px">
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import TheAppBar from '../src/components/TheAppBar.vue'
 export default {
   name: 'App',
-
+  components:{
+    TheAppBar
+  },
   data: () => ({
     //
   }),
