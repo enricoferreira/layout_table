76197adef4136012e290665b75133d41011be8ed
Enrico Ferreira <enricoferreira41@gmail.com>
2023-02-01 17:31:00 -0300

    ordenacao

--- table_template/src/components/TheTabelaHome.vue
+++ table_template/src/components/TheTabelaHome.vue
@@ -42,7 +42,17 @@
             <v-menu offset-y :close-on-content-click="false">
               <template v-slot:activator="{on, attrs}">
                 <th v-bind="attrs" v-on="on" class="text-left white py-2 px-2" style="color: #474747; font-size: 16px;position: sticky;
-            top: 3px;">Nome</th>
+            top: 3px;">
+                  <div class="d-flex justify-space-between">
+                    <div>
+                      Nome
+                    </div>
+                    <div @click.stop="ordenacao_sentido = ordenacao_sentido == 'cresc' ? 'desc' : 'cresc'">
+                      <v-icon @click.stop="ordenar('name', 'cresc')" size="19" class="mr-1" :color="ordenacao_sentido == 'cresc' && ordenacao_value == 'name' ? 'black' : ''">mdi-chevron-up</v-icon>
+                      <v-icon @click.stop="ordenar('name', 'desc')" size="19" :color="ordenacao_sentido == 'desc' && ordenacao_value == 'name' ? 'black' : ''">mdi-chevron-down</v-icon>
+                    </div>
+                  </div>
+                </th>
               </template>
               <v-sheet max-height="500" rounded class="px-1 py-2 pb-0 overflow-auto">
                 <v-text-field v-model="search_nome" placeholder="Pesquise por nome" dense hide-details outlined append-icon="mdi-close" @click:append="search_nome = ''"></v-text-field>
@@ -83,15 +93,15 @@
           </tr>
         </thead>
         <tbody>
-          <tr class="text-body-2" v-for="usu in usuarios.slice(0,100)" :key="usu.id">
+          <tr class="text-body-2" v-for="usu in usuarios_computed_filter.slice(0,100)" :key="usu.id">
             <td class="text-left py-2 px-2 text-truncate" style="max-width: 200px;width: 200px">{{usu.name}}</td>
             <td class="text-left py-2 px-2 text-truncate" style="max-width: 200px;width: 200px">{{usu.email}}</td>
             <td class="text-left py-2 px-2 text-truncate" style="max-width: 150px;width: 150px">{{usu.cargo}}</td>
             <td class="text-left py-2 px-2">{{usu.perfil_nome}}</td>
             <td class="text-center py-2 px-2">{{usu.data_ult_acesso | format_date}}</td>
             <td class="text-center py-2 px-2">{{usu.qtd_acesso}}</td>
-            <td class="text-center py-2 px-2">
-              {{usu.status_acesso}}
+            <td class="text-center py-2 px-2" @click.stop="usu.status_acesso = !usu.status_acesso">
+              <input :checked="usu.status_acesso == 1" type="checkbox" name="" id="" :value="usu.status_acessso">
               <!-- <div class="d-flex justify-center" style="width: 100%">
                 <v-checkbox dense hide-details class="mt-0" v-model="usu.status_acesso"></v-checkbox>
               </div> -->
@@ -127,9 +137,29 @@ import usuarios from '../api/usuarios'
         return this.usuarios.filter(usu => {
           return this.search_nome == '' ? true : usu.name.toLowerCase().includes(this.search_nome.toLowerCase())
         })
+      },
+      usuarios_computed_filter(){
+        return this.usuarios.filter(usu => {
+          return this.nomes_selected.includes(usu.name)
+        }).sort((a, b) => {
+          const a_format = a[this.ordenacao_value]
+          const b_format = b[this.ordenacao_value]
+
+          if (a_format > b_format) {
+              return this.ordenacao_sentido == 'cresc' ? 1 : -1;
+          }
+          if (a_format < b_format) {
+              return this.ordenacao_sentido == 'desc' ? 1 : -1;
+          }
+          return 0;
+        })
       }
     },
     methods: {
+      ordenar(value, sentido){
+        this.ordenacao_value = value;
+        this.ordenacao_sentido = sentido;
+      },
       toggleTodosNomes(){
         if(!this.selecionar_todos_nomes){
           this.nomes_selected = []
@@ -161,6 +191,10 @@ import usuarios from '../api/usuarios'
       selecionar_todos_nomes: true,
       search_nome: '',
 
+      // Ordenacao
+      ordenacao_value: '',
+      ordenacao_sentido: 'cresc',
+      
       shadow: false,
       // paginacao
       page: 1,
